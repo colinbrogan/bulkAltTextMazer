a27485b6ef97f2493d46a7fa4bd8b786029a7129
Colin Brogan <colinbrogan@gmail.com>
2016-01-26 16:40:31 -0600

    Trigger basic modal when clicking bookmarklet

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("I work.");
+$('#modal_container').show().append(
+[
+	'<header>',
+		'<h2 id="ModalTitle">Bulk Edit image alt text -- MAZER Hack</h2>',
+		'<a href="#" class="close-modal" role="button" aria-label="Close dialog">×</a>',
+	'</header>',
+	'<p>I work.</p>'
+].join("")
+); 
+$("#modal_backdrop").addClass("visible").attr("tabindex",0);
